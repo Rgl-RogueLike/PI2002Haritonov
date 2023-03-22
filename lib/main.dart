d3796a728dde63f2b10bb2d5105c63258dfefbf7
RogueLike <nikita.harit@mail.ru>
2023-03-22 12:07:37 +0300

    Лабораторная работа №5 Задание 1

--- lib/main.dart
+++ lib/main.dart
@@ -5,97 +5,30 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+    
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Список элементов',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Инкремент'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  void _resetCounter() {
-    setState((){
-      _counter = 0;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Значение инкремента:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            ButtonBar(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _decrementCounter,
-                  style: ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Colors.red), ),
-                  child: Text('-', style: TextStyle(fontSize: 40),),
-                ),
-                ElevatedButton(
-                  onPressed: _incrementCounter, 
-                    style: ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Colors.green),),
-                    child: Text('+', style: TextStyle(fontSize: 40),),
-                  )
-              ],
-            ),
-            ButtonBar(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextButton(
-                  onPressed: _resetCounter,
-                  child: Text('Сбросить', style: TextStyle(fontSize: 20, color: Colors.grey),),
-                  ),
-              ],
-            )
-          ],
+        primarySwatch: Colors.green,
         ),
-      ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Список элементов'),
+          ),
+          body: ListView(
+            children: const[
+              Text('0000'),
+              Divider(),
+              Text('0001'),
+              Divider(),
+              Text('0010'),
+            ],
+          ),
+        ),
     );
   }
 }
+
