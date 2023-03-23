dc2b9d56b360f49db2e105addf0e8826e4f0705d
RogueLike <nikita.harit@mail.ru>
2023-03-24 00:09:20 +0300

    Лабораторная работа №6

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,109 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
+class MyForm extends StatefulWidget {
+  const MyForm({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => MyFormState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyFormState extends State {
+  final _formKey = GlobalKey<FormState>();
+
+  int _width = 0;
+  int _height = 0;
+  int _square = 0;
+
   @override
   Widget build(BuildContext context){
     
-    return MaterialApp(
-      title: 'Список элементов',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Список элементов'),
-          ),
-          body: ListView.builder(
-            itemBuilder: (context, index) {
-              var num = pow(2, index);
-              return Text('2 ^ $index = $num');
-            }
+     return Container(
+      padding: const EdgeInsets.all(10.0),
+      child:
+        Form(
+          key: _formKey,
+          child: 
+            Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    const Text ('Ширина (мм):'),
+                    Expanded(
+                      child: 
+                        TextFormField( 
+                          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                          validator: (value){
+                            if (value!.isEmpty) return 'Задайте Ширину';
+                              _width = int.parse(value);
+                            }),
+                      ),
+                  ],
+                ),
+
+                Row(
+                  children: <Widget>[
+                    const Text ('Высоту (мм):'),
+                    Expanded(
+                      child: 
+                        TextFormField(
+                          inputFormatters: [FilteringTextInputFormatter.digitsOnly], 
+                          validator: (value){
+                            if (value!.isEmpty) return 'Задайте Высоту';
+                              _height = int.parse(value);
+                            }),
+                      ),
+                  ],
+                ),
+
+                const SizedBox(height: 20.0),
+
+                ElevatedButton(
+                  onPressed: (){
+                    if(_formKey.currentState!.validate()){
+                      setState(() {
+                        if(_height != 0 && _width != 0) _square = _width * _height;
+                      });
+                      String item = _square.toString();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text(item), backgroundColor: Colors.green,),
+                      );
+                    }
+                  },
+                  child:
+                    const Text('Посчитать', style: TextStyle(color: Colors.white)),
+                ), 
+                
+                const SizedBox(height: 50.0),
+
+                Column(
+                  children: [
+                    if(_square == 0)
+                      const Text('задайте параметр', style: TextStyle(fontSize: 25.0),)
+                    else
+                     Text('S = $_width * $_height = $_square (см2)', style: const TextStyle(fontSize: 25.0))
+                  ],
+                )
+                // Text(
+                //   {if(_square == 0)
+                //     'Задайте параметры'
+                //   else  'S = $_width * $_height = ${_area} (см2)'},
+                //   style: TextStyle(fontSize: 30.0),
+                // ),
+            ],
           ),
         ),
     );
   }
 }
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(title: const Text('Калькулятор площади')),
+      body: const MyForm()
+    )
+  )
+);
 
