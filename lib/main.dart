602f7ecde719906ee7086127c5c3740a2f8676eb
Rgl-RogueLike <nikita.harit@mail.ru>
2023-04-20 14:18:04 +0300

    lab7

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Инкремент'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  void _resetCounter() {
-    setState((){
-      _counter = 0;
-    });
-  }
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Значение инкремента:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            ButtonBar(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _decrementCounter,
-                  style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Colors.red), ),
-                  child: const Text('-', style: TextStyle(fontSize: 40),),
-                ),
-                ElevatedButton(
-                  onPressed: _incrementCounter, 
-                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Colors.green),),
-                    child: const Text('+', style: TextStyle(fontSize: 40),),
-                  )
-              ],
-            ),
-            ButtonBar(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextButton(
-                  onPressed: _resetCounter,
-                  child: const Text('Сбросить', style: TextStyle(fontSize: 20, color: Colors.grey),),
-                  ),
-              ],
-            )
-          ],
-        ),
-      ),
+      appBar: AppBar(title: const Text('Главное окно')),
+      body: Center(child: ElevatedButton(
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondScreen()));
+        },
+        child: const Text('Открыть второе окно')
+      ))
     );
   }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Второе окно')),
+      body: Center(child: ElevatedButton(
+        onPressed: (){
+          Navigator.pop(context);
+        },
+        child: const Text('Back'),
+      ),),
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: MainScreen(),
+  )
+  );
 }
