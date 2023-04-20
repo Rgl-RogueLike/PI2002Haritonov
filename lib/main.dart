afea327836234d8e7f0bc3d445fe772e62f32fc7
Rgl-RogueLike <nikita.harit@mail.ru>
2023-04-20 21:42:24 +0300

    8.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,144 @@
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'package:intl/intl.dart';
+import 'package:html/parser.dart';
 
-Future<List<Photo>> fetchPhotos(http.Client client) async {
-  final response = await client.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
-  return compute(parsePhotos, response.body);
+Future<List<News>> fetchNews(http.Client client) async {
+  final response = await client.get(Uri.parse('https://kubsau.ru/api/getNews.php?key=6df2f5d38d4e16b5a923a6d4873e2ee295d0ac90'));
+  Bidi.stripHtmlIfNeeded(response.body);
+  String html = parse(response.body).body!.text;
+  return compute(parseNews, html);
 }
 
-List<Photo> parsePhotos(String responseBody) {
+List<News> parseNews(String responseBody) {
   final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+  return parsed.map<News>((json) => News.fromJson(json)).toList();
 }
 
-class Photo {
-  final int albumId;
-  final int id;
+class News {
+  final String date;
   final String title;
-  final String url;
-  final String thumbnailUrl;
+  final String previewText;
+  final String image;
 
-  const Photo({
-    required this.albumId,
-    required this.id,
+  const News({
+    required this.date,
     required this.title,
-    required this.url,
-    required this.thumbnailUrl,
+    required this.previewText,
+    required this.image,
   });
 
-  factory Photo.fromJson(Map<String, dynamic> json) {
-    return Photo(
-      albumId: json['albumId'] as int,
-      id: json['id'] as int,
-      title: json['title'] as String,
-      url: json['url'] as String,
-      thumbnailUrl: json['thumbnailUrl'] as String,
+  factory News.fromJson(Map<String, dynamic> json) {
+    return News(
+      date: json['ACTIVE_FROM'] as String,
+      title: json['TITLE'] as String,
+      previewText: json['PREVIEW_TEXT'] as String,
+      image: json['PREVIEW_PICTURE_SRC'] as String,
     );
   }
 }
 
-void main() => runApp(const MyApp());
+void main() async {
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super (key: key);
 
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Фотогалерея';
+    const appTitle = 'Новоcти КубГау';
 
     return  const MaterialApp(
       title: appTitle,
-      home:MyHomePage(title: appTitle),
+      home:MyHomePage(title: appTitle)
     );
   }
 }
 
-class PhotoList extends StatelessWidget {
-  const PhotoList({Key? key, required this.photos}): super(key: key);
+class NewsList extends StatelessWidget {
+  const NewsList({Key? key, required this.news}): super(key: key);
 
-  final List<Photo> photos;
+  final List<News> news;
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        ), 
-      itemCount: photos.length,
-      itemBuilder: (context, index) {
-        return Image.network(photos[index].thumbnailUrl);
-      },
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: news.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding:  const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                      child: Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Column(
+                          children: [
+                            Image.network(news[index].image),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: Row(
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      '${news[index].date}',
+                                      style: TextStyle(color: Colors.grey),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: Row(
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      '${news[index].title}',
+                                      style: TextStyle(fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Divider(),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: Row(
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      '${news[index].previewText}',
+                                      textAlign: TextAlign.start,
+                                    ),
+                                  ),
+                                SizedBox(height: 10),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
@@ -86,16 +153,17 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+        backgroundColor: Colors.green,
       ),
-      body: FutureBuilder<List<Photo>>(
-        future: fetchPhotos(http.Client()),
+      body: FutureBuilder<List<News>>(
+        future: fetchNews(http.Client()),
         builder: (context, snapshot) {
           if(snapshot.hasError) {
             return const Center(
               child: Text('Ошибка запроса!'),
             );
           } else if (snapshot.hasData) {
-            return PhotoList(photos: snapshot.data!);
+            return NewsList(news: snapshot.data!);
           } else {
             return const Center(
               child: CircularProgressIndicator(),
@@ -104,5 +172,14 @@ class MyHomePage extends StatelessWidget {
         }
       ),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = 
+        (X509Certificate cert, String host, int port) => true;
   }
 }
